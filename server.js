d3783576f930d935acc8db0920f51d01653111b4
akrGIT <akr.nahim@gmail.com>
2020-10-15 15:24:28 +0100

    Https methods / Restful APIs

--- server.js
+++ server.js
@@ -10,11 +10,7 @@ const todos = [
 ];
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'application/json',
-    'X-Powered-By': 'Node.js',
-  });
-
+  const { method, url } = req;
   let body = [];
   req
     .on('data', (chunck) => {
@@ -22,15 +18,40 @@ const server = http.createServer((req, res) => {
     })
     .on('end', () => {
       body = Buffer.concat(body).toString();
-      console.log(body);
-    });
 
-  res.end(
-    JSON.stringify({
-      success: true,
-      data: todos,
-    })
-  );
+      let status = 404;
+      const response = {
+        success: false,
+        data: null,
+        error: null,
+      };
+
+      // Check request
+      if (method === 'GET' && url === '/todos') {
+        status = 200;
+        response.success = true;
+        response.data = todos;
+      } else if (method === 'POST' && url === '/todos') {
+        const { id, text } = JSON.parse(body);
+        // Some validation
+        if (!id || !text) {
+          status = 400;
+          response.error = 'Please enter Id and Text';
+        } else {
+          todos.push({ id, text });
+          status = 201;
+          response.success = true;
+          response.data = todos;
+        }
+      }
+
+      res.writeHead(status, {
+        'Content-Type': 'application/json',
+        'X-Powered-By': 'Node.js',
+      });
+
+      res.end(JSON.stringify(response));
+    });
 });
 
 const PORT = 5000;
