3f4fda09bc20e0fdf4f010a18148532c8f8a6f0f
akrGIT <akr.nahim@gmail.com>
2020-11-01 17:51:24 +0100

    Second Commits : Creating routes and using Express Router

--- DevCamperAPI/server.js
+++ DevCamperAPI/server.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+// Route file
+const bootcamps = require('./routes/bootcamps');
+
 // Load en vars
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
+// Mount router
+app.use('/api/v1/bootcamps', bootcamps);
+
 // liten to server
 const PORT = process.env.PORT || 5000;
+
 app.listen(
   PORT,
   console.log(`server running in ${process.env.NODE_ENV} snode on port ${PORT}`)
