e73c985c00323bb7a03635e392481306fa733e03
akrGIT <akr.nahim@gmail.com>
2020-11-01 18:17:49 +0100

    Third commit : Creating controllers methods

---
+++ DevCamperAPI/controllers/bootcamps.js
@@ -0,0 +1,37 @@
+// @desc        Get all bootcamps
+// @route       GET /api/v1/bootcamps
+// @access      Public
+exports.getBootcamps = (req, res, next) => {
+  res.status(200).json({ success: true, msg: `Show all bootcampers` });
+};
+// @desc        Get single bootcamp
+// @route       GET /api/v1/bootcamps/:id
+// @access      Public
+exports.getBootcamp = (req, res, next) => {
+  res.status(200).json({
+    success: true,
+    msg: `Display bootcamp ${req.params.id}`,
+  });
+};
+// @desc        Create new bootcamp
+// @route       POST /api/v1/bootcamps
+// @access      Private
+exports.addBootcamp = (req, res, next) => {
+  res.status(200).json({ success: true, msg: `Create new bootcamp` });
+};
+// @desc        Update a bootcamp
+// @route       PUT /api/v1/bootcamps/:id
+// @access      Private
+exports.updateBootcamp = (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Update bootcamp ${req.params.id}` });
+};
+// @desc        Delete a bootcamp
+// @route       DELETE /api/v1/bootcamps/:id
+// @access      Private
+exports.deleteBootcamp = (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
+};
--- DevCamperAPI/routes/bootcamps.js
+++ DevCamperAPI/routes/bootcamps.js
@@ -1,32 +1,19 @@
 const express = require('express');
+// Require methods from bootcamps.js (controller) usong desctructring
+const {
+  getBootcamps,
+  getBootcamp,
+  addBootcamp,
+  updateBootcamp,
+  deleteBootcamp,
+} = require('../controllers/bootcamps');
+
 const router = express.Router();
-/**
- *Defining routes
- */
-router.get('/', (req, res) => {
-  res.status(200).json({ success: true, msg: `Show all bootcampers` });
-});
-
-router.get('/:id', (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Display bootcamp ${req.params.id}` });
-});
-
-router.post('/', (req, res) => {
-  res.status(200).json({ success: true, msg: `Create new bootcamp` });
-});
-
-router.put('/:id', (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Update bootcamp ${req.params.id}` });
-});
-
-router.delete('/:id', (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
-});
+router.route('/').get(getBootcamps).post(addBootcamp);
+router
+  .route('/:id')
+  .get(getBootcamp)
+  .put(updateBootcamp)
+  .delete(deleteBootcamp);
 
 module.exports = router;
