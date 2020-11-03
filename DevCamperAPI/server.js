39641e2777a8f870de35c1bea9ac3d27517d4d56
akrGIT <akr.nahim@gmail.com>
2020-11-03 20:39:05 +0100

    Fifth commit : Setting up Mongoose node package as abstracted layer for custom middlware, modelsn & validations

--- DevCamperAPI/server.js
+++ DevCamperAPI/server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
+
+// Load env vars
+dotenv.config({ path: './config/config.env' });
+
+// COnnect to database
+connectDB();
+
 // Route file
 const bootcamps = require('./routes/bootcamps');
-
-// Load en vars
-dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
@@ -20,7 +25,14 @@ app.use('/api/v1/bootcamps', bootcamps);
 // liten to server
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`server running in ${process.env.NODE_ENV} snode on port ${PORT}`)
 );
+
+// Global Handler to handle the unhandled promise rejections
+process.on('unhandledRejection', (error, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server & exit process
+  server.close(() => process.exist(1));
+});
